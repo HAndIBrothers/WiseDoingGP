0574d1ae8f46389f4bbb9bd7a1932438eb7b9404
ryusiin <risnworlds@gmail.com>
2019-06-11 09:51:45 +0900

    [Wise Doing GP] for Error Check

---
+++ app/build/generated/source/buildConfig/debug/bplant/gp/wisedoing_git/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package bplant.gp.wisedoing_git;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "bplant.gp.wisedoinggit";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 16;
+  public static final String VERSION_NAME = "0.16";
+}
---
+++ app/build/generated/source/buildConfig/release/bplant/gp/wisedoing_git/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package bplant.gp.wisedoing_git;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "bplant.gp.wisedoinggit";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 15;
+  public static final String VERSION_NAME = "0.15";
+}
